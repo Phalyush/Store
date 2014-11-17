c4b9262d67ac2ae7dac1e8f3d6881c4cda86b822
Phalyush <Phalyush@gmail.com>
2014-11-17 15:43:17 +0200

    новый

--- Header.master.cs
+++ Header.master.cs
@@ -7,6 +7,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.Generic;
 public partial class Header : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -15,16 +16,19 @@ public partial class Header : System.Web.UI.MasterPage
     }
     protected void Enter_Click(object sender, EventArgs e)
     {
-        EnterIntoAdminPage Entrance = new EnterIntoAdminPage(EnterLogin.Text, EnterPassword.Text);
-        if (Entrance.Autorization())
-        {
-            HttpCookie adminAuth = new HttpCookie("adminUser", EnterLogin.Text);
-            
-            Response.Redirect("~/AdminPage.aspx");
-        }
-        
+        IDictionary<string, string> paramseters = new Dictionary<string, string>();
+        paramseters.Add("Login", EnterLogin.Text);
+        paramseters.Add("Password", EnterPassword.Text);
+        string query = "SELECT * FROM Seller where [Login]=@Login and [Password]=@Password";
+        SqlHelp sh = new SqlHelp();
+
+        if (sh.ExecuteQuery(query, paramseters).Rows.Count==0)
         {
             error.Visible = true;
+        }
+        else
+        {
+            Response.Redirect("AdminPage.aspx");
         }
     }
    
